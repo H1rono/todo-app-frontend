35caba0c187cf7c297641fc21c12cb07b5d9ccb0
H1rono <hronok66@gmail.com>
2023-04-04 09:23:35 +0900

    :heavy_plus_sign: Add vitest and coverage-c8

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    test: {
+        coverage: {
+            provider: "c8",
+            reportsDirectory: "coverage",
+            reporter: "lcovonly",
+        },
+    },
 });
