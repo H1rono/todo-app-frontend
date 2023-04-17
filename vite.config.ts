452740faa8b0c04555e1a07a25f22fe9e3538837
H1rono <hronok66@gmail.com>
2023-04-17 18:22:00 +0900

    :wrench: Configure CORS

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dns from "node:dns";
+
+dns.setDefaultResultOrder("ipv4first");
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        host: "0.0.0.0",
+        proxy: {
+            "/api": {
+                target: process.env.VITE_API_URL || "http://localhost:3000",
+                changeOrigin: true,
+            },
+        },
+    },
     test: {
         coverage: {
             provider: "c8",
